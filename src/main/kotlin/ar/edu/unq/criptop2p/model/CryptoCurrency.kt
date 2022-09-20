05a5912cd08ae27196f95e8cbe6fcf0f56929f53
Matias Fuentes <matiasnfuentes@gmail.com>
2022-09-20 19:05:54 -0300

    get cryptocurrency last day prices

--- src/main/kotlin/ar/edu/unq/criptop2p/model/CryptoCurrency.kt
+++ src/main/kotlin/ar/edu/unq/criptop2p/model/CryptoCurrency.kt
@@ -1,29 +1,8 @@
 package ar.edu.unq.criptop2p.model
 
 import java.util.*
-import javax.persistence.*
 
-@Entity
-class CryptoCurrency(@Column
-                     private val price: Double,
-                     @Column
-                     private val symbol: String) {
-
-    @Id
-    @SequenceGenerator(
-            name = "cryptoCurrency_sequence",
-            sequenceName = "cryptoCurrency_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "cryptoCurrency_sequence"
-    )
-    @Column
-    private val id: Long? = null
-    @Column
-    private val timestamp: Date = Date()
-
+class CryptoCurrency(private val price: Double, private val symbol: String, private val date: Date = Date() ) {
 
     fun getPrice(): Double = this.price
     fun getSymbol(): String = this.symbol
