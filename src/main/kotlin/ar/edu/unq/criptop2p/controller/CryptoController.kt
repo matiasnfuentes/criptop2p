af5827526f2dcc41cb282c5ff22e515fe8b23f55
Daniel Barberena <Daniel.Barberena10@gmail.com>
2022-10-01 17:07:11 -0300

    Add Swagger Documentation

--- src/main/kotlin/ar/edu/unq/criptop2p/controller/CryptoController.kt
+++ src/main/kotlin/ar/edu/unq/criptop2p/controller/CryptoController.kt
@@ -2,6 +2,7 @@ package ar.edu.unq.criptop2p.controller
 
 import ar.edu.unq.criptop2p.model.CryptoCurrency
 import ar.edu.unq.criptop2p.service.CryptoService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,14 +16,16 @@ class CryptoController (
     @Autowired
     private val cryptoService: CryptoService){
 
-    @GetMapping("/prices")
-    fun prices(): ResponseEntity<List<CryptoCurrency>> {
-        return ResponseEntity.ok().body(this.cryptoService.getCryptoPrices())
-    }
-
+    @Operation(summary="API to list the 24hs prices of a cryptocurrency", description="This endpoint list the last 24hs prices of the cryptocurrency send by parameter.")
     @GetMapping("/price/{symbol}")
     fun last24HsPrices(@PathVariable symbol:String): ResponseEntity<List<CryptoCurrency>> {
         return ResponseEntity.ok().body(this.cryptoService.getLast24HsPrices(symbol))
+    }
+
+    @Operation(summary="API to list current cryptocurrency prices", description="This endpoint list the current price of all the cryptocurrencies supported by the platform.")
+    @GetMapping("/prices")
+    fun prices(): ResponseEntity<List<CryptoCurrency>> {
+        return ResponseEntity.ok().body(this.cryptoService.getCryptoPrices())
     }
 
 }
