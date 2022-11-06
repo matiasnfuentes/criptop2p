5a009f46308ec0319a0d62670061bb9f04c7b123
Daniel Barberena <Daniel.Barberena10@gmail.com>
2022-11-06 14:07:04 -0300

    Add controller logging with aspects

---
+++ src/main/kotlin/ar/edu/unq/criptop2p/aspect/LogControllerExecution.kt
@@ -0,0 +1,5 @@
+package ar.edu.unq.criptop2p.aspect
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LogControllerExecution
